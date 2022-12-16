47f98cd76ffb540a17a3e76152a1e72f17895375
sidsitaula <siddharthasitaula02@gmail.com>
2022-12-16 14:25:39 +0545

    timer

---
+++ l6/timer.py
@@ -0,0 +1,70 @@
+import datetime
+import time
+
+
+class TimedEvent:
+    def __init__(self, endtime, callback):
+        self.endtime = endtime
+        self.callback = callback
+
+    def ready(self):
+        return self.endtime <= datetime.datetime.now()
+
+
+class Timer:
+    def __init__(self):
+        self.events = []
+
+    def call_after(self, delay, callback):
+        end_time = datetime.datetime.now() + \
+            datetime.timedelta(seconds=delay)
+
+        self.events.append(TimedEvent(end_time, callback))
+
+    def run(self):
+        while True:
+            ready_events = (e for e in self.events if e.ready())
+            for event in ready_events:
+                event.callback(self)
+                self.events.remove(event)
+            time.sleep(0.5)
+
+
+def format_time(message, *args):
+    now = datetime.datetime.now().strftime("%I:%M:%S")
+    print(message.format(*args, now=now))
+
+
+def one(timer):
+    format_time("{now}: Called One")
+
+
+def two(timer):
+    format_time("{now}: Called Two")
+
+
+def three(timer):
+    format_time("{now}: Called Three")
+
+
+class Repeater:
+    def __init__(self):
+        self.count = 0
+
+    def repeater(self, timer):
+        format_time("{now}: repeat {0}", self.count)
+        self.count += 1
+        timer.call_after(5, self.repeater)
+
+
+timer = Timer()
+timer.call_after(1, one)
+timer.call_after(2, one)
+timer.call_after(2, two)
+timer.call_after(4, two)
+timer.call_after(3, three)
+timer.call_after(6, three)
+repeater = Repeater()
+timer.call_after(5, repeater.repeater)
+format_time("{now}: Starting", "wabaaaa")
+timer.run()
