b0858b81c1415f0ede5e700bfe61bf12d70dd563
sidsitaula <siddharthasitaula02@gmail.com>
2022-12-17 21:47:39 +0545

    auto updater

---
+++ l7/numeric.py
@@ -0,0 +1,11 @@
+# create a variable with integer value.
+a = 100
+print("The type of variable having value", a, " is ", type(a))
+
+# create a variable with float value.
+b = 10.2345
+print("The type of variable having value", b, " is ", type(b))
+
+# create a variable with complex value.
+c = 100+3j
+print("The type of variable having value", c, " is ", type(c))
