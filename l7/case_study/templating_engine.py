df0cef271db6d878aa5c8c7c82a621695f752dca
sidsitaula <siddharthasitaula02@gmail.com>
2022-12-18 16:49:03 +0545

    templating engine

--- l7/case_study/templating_engine.py
+++ l7/case_study/templating_engine.py
@@ -20,10 +20,46 @@ class TemplateEngine:
             self.context = json.load(contextfile)
 
     def process(self):
-        print("PROCESSING...")
+        match = DIRECTIVE_RE.search(self.template, pos=self.pos)
+        while match:
+            self.outfile.write(self.template[self.pos:match.start()])
+            directive, arguement = match.groups()
+            method_name = 'process_{}'.format(directive)
+            getattr(self, method_name)(match, arguement)
+            match = DIRECTIVE_RE.search(self.template, pos=self.pos)
+        self.outfile.write(self.template[self.pos:])
+
+    def process_include(self, match, argument):
+        with (self.working_dir / argument).open() as includefile:
+            self.outfile.write(includefile.read())
+        self.pos = match.end()
+
+    def process_variable(self, match, argument):
+        self.outfile.write(self.context.get(argument, ''))
+        self.pos = match.end()
+
+    def process_loopover(self, match, argument):
+        self.loop_index = 0
+        self.loop_list = self.context.get(argument, [])
+        self.pos = self.loop_pos = match.end()
+
+    def process_loopvar(self, match, argument):
+        self.outfile.write(self.loop_list[self.loop_index])
+        self.pos = match.end()
+
+    def process_endloop(self, match, argument):
+        self.loop_index += 1
+        if self.loop_index >= len(self.loop_list):
+            self.pos = match.end()
+            del self.loop_index
+            del self.loop_list
+            del self.loop_pos
+        else:
+            self.pos = self.loop_pos
 
 
 if __name__ == '__main__':
-    infilename, outfilename, contextfilename = sys.argv[1:]
+    infilename, outfilename, contextfilename = [
+        'template.html', 'out.html', 'context.json']
     engine = TemplateEngine(infilename, outfilename, contextfilename)
     engine.process()
